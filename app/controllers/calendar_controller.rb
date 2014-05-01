a5023c3c531402e685b47e9062e8f0ad48a949d0
Zach Bolin <zbolin@ksu.edu>
2014-05-01 14:53:48 -0500

    add calendar files

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,23 +1,26 @@
 class CalendarController < ApplicationController
 
   def index
-    @todays=[]
-    all_evs = Event.all
-    all_evs.each do |sing_ev|
-      if sing_ev.start_at > Time.now
-        @todays << sing_ev
-      end
-    end
-    @event = Event.new
-    @month = (params[:month] || (Time.zone || Time).now.month).to_i
-    @year = (params[:year] || (Time.zone || Time).now.year).to_i
+#    @todays=[]
+#    all_evs = Event.all
+#    all_evs.each do |sing_ev|
+#      if sing_ev.start_at > Time.now
+#        @todays << sing_ev
+#      end
+#    end
+#    @event = Event.new
+#    @month = (params[:month] || (Time.zone || Time).now.month).to_i
+#    @year = (params[:year] || (Time.zone || Time).now.year).to_i
 
-    @shown_month = Date.civil(@year, @month)
+#    @shown_month = Date.civil(@year, @month)
 
-    @event_strips = Event.event_strips_for_month(@shown_month)
+#    @event_strips = Event.event_strips_for_month(@shown_month)
+   redirect_to :action => :show
   end
 
+  def hello
+  end
   def show
-    @event = Event.find(params[:id])
+#    @event = Event.find(params[:id])
   end
 end
